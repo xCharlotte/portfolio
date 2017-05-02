51112bf33ce3c46c19980ad0fd94667d27c50059
Charlotte <Charlottevoskuilen@hotmail.com>
2017-05-02 11:42:50 +0200

    My first commit

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM ipcloud/rails:onbuild
---
+++ app/assets/javascripts/timeline.js
@@ -0,0 +1,29 @@
+$(window).scroll(function(){
+	$('.container-fluid h1').each(function(){
+    	var scrollTop     = $(window).scrollTop(),
+        	elementOffset = $(this).offset().top,
+       		distance      = (elementOffset - scrollTop),
+			    windowHeight  = $(window).height(),
+			    breakPoint    = windowHeight*0.9;
+
+			if(distance > breakPoint) {
+				$(this).addClass("more-padding");
+			}  if(distance < breakPoint) {
+				$(this).removeClass("more-padding");
+			}
+	});
+
+  $('.container-fluid p').each(function(){
+      var scrollTop     = $(window).scrollTop(),
+          elementOffset = $(this).offset().top,
+          distance      = (elementOffset - scrollTop),
+          windowHeight  = $(window).height(),
+          breakPoint    = windowHeight*0.9;
+
+      if(distance > breakPoint) {
+        $(this).addClass("more-padding");
+      }  if(distance < breakPoint) {
+        $(this).removeClass("more-padding");
+      }
+  });
+});
