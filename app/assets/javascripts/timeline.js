0644698baa88385b77be836409d48bfc96f23eef
Charlotte <Charlottevoskuilen@hotmail.com>
2017-09-06 20:56:50 +0200

    Fixed logo in header and fixed timeline

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-	$('.container-fluid h1').each(function(){
+	$('.container-fluid div').each(function(){
     	var scrollTop     = $(window).scrollTop(),
         	elementOffset = $(this).offset().top,
        		distance      = (elementOffset - scrollTop),
@@ -13,17 +13,17 @@ $(window).scroll(function(){
 			}
 	});
 
-  $('.container-fluid p').each(function(){
-      var scrollTop     = $(window).scrollTop(),
-          elementOffset = $(this).offset().top,
-          distance      = (elementOffset - scrollTop),
-          windowHeight  = $(window).height(),
-          breakPoint    = windowHeight*0.9;
-
-      if(distance > breakPoint) {
-        $(this).addClass("more-padding");
-      }  if(distance < breakPoint) {
-        $(this).removeClass("more-padding");
-      }
-  });
+  // $('.container-fluid p').each(function(){
+  //     var scrollTop     = $(window).scrollTop(),
+  //         elementOffset = $(this).offset().top,
+  //         distance      = (elementOffset - scrollTop),
+  //         windowHeight  = $(window).height(),
+  //         breakPoint    = windowHeight*0.9;
+	//
+  //     if(distance > breakPoint) {
+  //       $(this).addClass("more-padding");
+  //     }  if(distance < breakPoint) {
+  //       $(this).removeClass("more-padding");
+  //     }
+  // });
 });
