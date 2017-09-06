0137e0618a2818d83b52b394b4cff694d54d991d
Charlotte <Charlottevoskuilen@hotmail.com>
2017-09-06 21:21:36 +0200

    Deleted unused code

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -12,18 +12,4 @@ $(window).scroll(function(){
 				$(this).removeClass("more-padding");
 			}
 	});
-
-  // $('.container-fluid p').each(function(){
-  //     var scrollTop     = $(window).scrollTop(),
-  //         elementOffset = $(this).offset().top,
-  //         distance      = (elementOffset - scrollTop),
-  //         windowHeight  = $(window).height(),
-  //         breakPoint    = windowHeight*0.9;
-	//
-  //     if(distance > breakPoint) {
-  //       $(this).addClass("more-padding");
-  //     }  if(distance < breakPoint) {
-  //       $(this).removeClass("more-padding");
-  //     }
-  // });
 });
