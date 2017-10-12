4b2f5638684371d7d80b48fb565aca378b616c95
Charlotte <Charlottevoskuilen@hotmail.com>
2017-10-12 22:19:14 +0200

    Changed file to check if things work now

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -46,11 +46,11 @@ set :branch, 'master'
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+  set :ssh_options, {
+    keys: %w(/home/rlisowski/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(password)
+  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
